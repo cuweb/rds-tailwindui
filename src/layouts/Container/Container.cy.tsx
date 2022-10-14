41170826f6cb998fa48cf9ff63b3d9d3720d492c
Ish <ishdeep.711@gmail.com>
2022-10-14 12:50:00 -0400

    adding test for layouts as well

--- src/layouts/Container/Container.cy.tsx
+++ src/layouts/Container/Container.cy.tsx
@@ -1,7 +1,43 @@
-describe('Button', () => {
-  it('Does Button exist?', () => {
-    cy.visit('/iframe.html?id=container--default&viewMode=story');
-    cy.get('button').should('exist');
+describe('Container', () => {
+  //default
+  it(' Container Default with White Background ', () => {
+    cy.visit('/iframe.html?id=layouts-container--default');
+    cy.get('div')
+      .should('have.class', 'cu-container')
+      .and('have.class', 'bg-white');
+    cy.get('p').contains('Empty Container');
+  });
+
+  //Container with grey Background
+  it(' Container Grey background', () => {
+    cy.visit('/iframe.html?id=layouts-container--gray-background');
+    cy.get('div')
+      .should('have.class', 'cu-container')
+      .and('have.class', 'bg-cu-black-50');
+    cy.get('p').contains('Gray Container');
+  });
+
+  //Container with two column
+  it('Container with two column ', () => {
+    cy.visit('/iframe.html?id=layouts-container--with-two-columns');
+    cy.get('div')
+      .should('have.class', 'cu-container')
+      .and('have.class', 'bg-cu-black-50');
+    cy.get('div')
+      .should('have.class', 'cu-column')
+      .and('have.class', 'md:grid-cols-2');
+  });
+
+  //layouts-container--with-panels
+  it('Container with two column Panel', () => {
+    cy.visit('/iframe.html?id=layouts-container--with-panels');
+    cy.get('div')
+      .should('have.class', 'cu-column')
+      .and('have.class', 'md:grid-cols-2');
+    cy.get('div')
+      .should('have.class', 'cu-panel')
+      .and('have.class', 'shadow-lg');
+    cy.get('h2').contains('Panel Header');
   });
 });
 export {};
