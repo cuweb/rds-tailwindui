1414e1b8b57716a41bdddfb478186d56011a3fbb
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-10-17 10:40:02 -0400

    update test

--- src/components/Filter/Filter.cy.tsx
+++ src/components/Filter/Filter.cy.tsx
@@ -1,7 +1,11 @@
-describe('Button', () => {
-  it('Does Button exist?', () => {
-    cy.visit('/iframe.html?id=input-button--default&viewMode=story');
+describe('Filter', () => {
+  //default
+  it('Default Filter exist?', () => {
+    cy.visit('/iframe.html?id=components-filter--default');
     cy.get('button').should('exist');
+    cy.get('h2').should('exist');
+    cy.get('h3').should('exist');
+    cy.get('span').should('exist');
   });
 });
 export {};
