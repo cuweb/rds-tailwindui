4a0b5ca176244bd2b6f37dfa7b3be05019a32c8c
Ish <ishdeep.711@gmail.com>
2022-10-18 16:41:47 -0400

    created the button component, still need to write test

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Button, ButtonProps } from './Button';
+import { PlusIcon } from '@heroicons/react/20/solid';
 
 const meta: Meta = {
   title: 'Components/Input/Button',
@@ -31,4 +32,74 @@ export const Default = Template.bind({});
 Default.args = {
   title: 'Storybook Button',
   disabled: false,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const GhostButton = Template.bind({});
+GhostButton.args = {
+  title: 'Storybook Button',
+  disabled: false,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+  ghost: true,
+};
+
+export const GreyButton = Template.bind({});
+GreyButton.args = {
+  title: 'Storybook Button',
+  grey: true,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const WhiteButton = Template.bind({});
+WhiteButton.args = {
+  title: 'Storybook Button',
+  white: true,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const FullWidth = Template.bind({});
+FullWidth.args = {
+  title: 'Storybook Button',
+  full: true,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const centeredButton = Template.bind({});
+centeredButton.args = {
+  title: 'Storybook Button',
+  center: true,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const ChildrenInButton: Story = args => (
+  <Button {...args}>
+    <PlusIcon className="-ml-0.5 h-4 w-4" aria-hidden="true" />
+    Sample Button
+  </Button>
+);
+ChildrenInButton.args = {
+  onClick: () => {
+    alert('button has been clicked ');
+  },
+};
+
+export const disabledButton = Template.bind({});
+disabledButton.args = {
+  title: 'Storybook Button',
+  disabled: true,
+  onClick: () => {
+    alert('button has been clicked ');
+  },
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,55 @@
+import PlusIcon from '@heroicons/react/20/solid/PlusIcon';
 import React from 'react';
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-  title: string;
+  title?: string;
+  ghost?: boolean;
+  full?: boolean;
+  center?: boolean;
+  children?: React.ReactNode;
+  grey?: boolean;
+  white?: boolean;
 }
 
-export const Button = ({ disabled, title, ...rest }: ButtonProps) => (
-  <button
-    className={`bg-transparent text-gray-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded disabled:text-gray-400 disabled:border-gray-400 disabled:cursor-default ${!disabled &&
-      'hover:bg-gray-500'}`}
-    disabled={disabled}
-    {...rest}
-  >
-    {title}
-  </button>
-);
+const styles = {
+  default: `items-center inline-flex rounded-md  px-4 py-3 font-medium shadow-sm focus:outline-none`,
+  normal: `bg-cu-red hover:bg-cu-black text-white border border-transparent`,
+  ghost: `border-cu-red text-cu-red bg-white border border-1 hover:bg-cu-red hover:text-white`,
+  grey: `border border-transparent text-cu-black bg-slate-100 hover:bg-cu-black hover:text-white`,
+  white: `border border-1 border-slate-200 text-cu-black bg-white hover:bg-cu-black hover:text-white`,
+  disabled: 'disabled:border-gray-400 disabled:cursor-default',
+};
 
-// export default Button;
+export const Button = ({
+  disabled,
+  title,
+  ghost,
+  full,
+  center,
+  white,
+  children,
+  grey,
+  ...rest
+}: ButtonProps) => {
+  const isGhost = ghost ? styles.ghost : '';
+  const isGrey = grey ? styles.grey : '';
+  const isWhite = white ? styles.white : '';
+  const isFull = full ? 'w-full' : '';
+  const isCenter = center ? 'relative left-1/2 -translate-x-1/2 ' : '';
+
+  return (
+    <button
+      type="button"
+      className={`${styles.default} ${!ghost &&
+        !grey &&
+        !white &&
+        styles.normal} ${isGhost} ${isGrey} ${isWhite} ${isFull} ${isCenter}`}
+      disabled={disabled}
+      {...rest}
+    >
+      {children}
+      {title}
+    </button>
+  );
+};
