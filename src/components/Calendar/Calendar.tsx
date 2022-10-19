850d8dc91e3d210687d9e0674b746b9b7c81f312
HanqingZhou <72098908+HanqingZhou@users.noreply.github.com>
2022-10-19 12:10:46 -0400

    update calendar style

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,5 +1,3 @@
-import { Menu, Transition } from '@headlessui/react';
-import { EllipsisVerticalIcon } from '@heroicons/react/24/outline';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
 import {
   add,
@@ -17,7 +15,7 @@ import {
   startOfToday,
 } from 'date-fns';
 import React from 'react';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 export interface CalendarProps {
   meetings: {
@@ -63,10 +61,6 @@ export const Calendar: React.FC<CalendarProps> = ({
     const firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
     setCurrentMonth(format(firstDayNextMonth, 'MMM-yyyy'));
   };
-
-  const selectedDayMeetings = meetings.filter(meeting =>
-    isSameDay(parseISO(meeting.startDatetime), selectedDay)
-  );
 
   return (
     <div className="pt-16">
@@ -152,109 +146,15 @@ export const Calendar: React.FC<CalendarProps> = ({
                 </div>
               ))}
             </div>
-          </div>
-          <section className="mt-12 md:mt-0 md:pl-14">
-            <h2 className="font-semibold text-cu-black-900">
-              Schedule for{' '}
+            <h2 className="text-center font-semibold text-cu-black-900">
+              Selected{' '}
               <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>
                 {format(selectedDay, 'MMM dd, yyy')}
               </time>
             </h2>
-            <ol className="mt-4 space-y-1 text-sm leading-6 text-cu-black-500">
-              {selectedDayMeetings.length > 0 ? (
-                selectedDayMeetings.map(meeting => (
-                  <Meeting meeting={meeting} key={meeting.id} />
-                ))
-              ) : (
-                <p>No meetings for today.</p>
-              )}
-            </ol>
-          </section>
+          </div>
         </div>
       </div>
     </div>
-  );
-};
-
-const Meeting = ({ meeting }: any) => {
-  const startDateTime = parseISO(meeting.startDatetime);
-  const endDateTime = parseISO(meeting.endDatetime);
-
-  return (
-    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-cu-black-100 hover:bg-cu-black-100">
-      <img
-        src={meeting.imageUrl}
-        alt=""
-        className="flex-none w-10 h-10 rounded-full"
-      />
-      <div className="flex-auto">
-        <p className="text-cu-black-900">{meeting.name}</p>
-        <p className="mt-0.5">
-          <time dateTime={meeting.startDatetime}>
-            {format(startDateTime, 'h:mm a')}
-          </time>{' '}
-          -{' '}
-          <time dateTime={meeting.endDatetime}>
-            {format(endDateTime, 'h:mm a')}
-          </time>
-        </p>
-      </div>
-      <Menu
-        as="div"
-        className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
-      >
-        <div>
-          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-cu-black-500 hover:text-cu-black-600">
-            <span className="sr-only">Open options</span>
-            <EllipsisVerticalIcon className="w-6 h-6" aria-hidden="true" />
-          </Menu.Button>
-        </div>
-
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active
-                        ? 'bg-cu-black-100 text-cu-black-900'
-                        : 'text-cu-black-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active
-                        ? 'bg-cu-black-100 text-cu-black-900'
-                        : 'text-cu-black-700',
-                      'block px-4 py-2 text-sm'
-                    )}
-                  >
-                    Cancel
-                  </a>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-    </li>
   );
 };
