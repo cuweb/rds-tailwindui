6e9c9ef90cb7570ce755c1322ba2eddd91680446
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-20 10:25:34 -0400

    Updated stories

--- src/components/Filter/Filter.stories.tsx
+++ src/components/Filter/Filter.stories.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Filter, FilterProps } from './Filter';
 import data from './FilterData.json';
-//console.log(data.filters);
-const meta: Meta = {
-  title: 'Components/Filter',
-  component: Filter,
-};
 
-export default meta;
+export default {
+  title: 'Components/Input/Filter',
+  component: Filter,
+  argTypes: {},
+} as Meta<typeof Filter>;
 
 const Template: Story<FilterProps> = args => <Filter {...args} />;
 
