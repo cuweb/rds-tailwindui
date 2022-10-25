781f67271e7a99c05906525dca3a5ca911d651c1
Niranjith Perumal <niranjithperumal@cunet.carleton.ca>
2022-10-25 09:22:54 -0400

    refactor(build): move assets folder on build

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ module.exports = {
   theme: {
     extend: {
         backgroundImage: {
-            'cu-waves-red': "url('/cu-waves-red.svg')",
-            'cu-waves-white-20': "url('/cu-waves-white-20.svg')",
+            'cu-waves-red': "url('/assets/cu-waves-red.svg')",
+            'cu-waves-white-20': "url('/assets/cu-waves-white-20.svg')",
         },
         colors: {
           'cu-red': {
