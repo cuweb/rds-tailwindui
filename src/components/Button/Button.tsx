3de2accca312c743ea8b99dd2244aaf4db41132b
Troy Chaplin <troy.chaplin@gmail.com>
2022-10-26 18:54:31 -0400

    Added gap to create space between when icon is used

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,7 +16,7 @@ export interface ButtonProps
 }
 
 const styles = {
-  core: `inline-flex items-center justify-center font-medium rounded-md focus:outline-none`,
+  core: `inline-flex gap-1 items-center justify-center font-medium rounded-md focus:outline-none`,
   default: `bg-cu-red text-white hover:bg-cu-black-600`,
   ghost: `border border-1 border-cu-red text-cu-red bg-white hover:bg-cu-red hover:text-white`,
   grey: `text-cu-black-800 bg-cu-black-50 hover:bg-cu-black-600 hover:text-white`,
