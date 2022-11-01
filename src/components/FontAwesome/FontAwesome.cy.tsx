37b492c766afa2c6e4fbcbb9ca2e021dbd8d9d43
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-01 10:59:40 -0400

    Removed font awesome files, issue with babel to look into later

--- src/components/FontAwesome/FontAwesome.cy.tsx
+++
@@ -1,9 +0,0 @@
-describe('FontAwesome', () => {
-  //default
-  it('Should render the icon', () => {
-    cy.clearCookies();
-    cy.visit(`/iframe.html?id=components-icons-fontawesome`);
-    cy.get('svg').should('exist');
-  });
-});
-export {};
