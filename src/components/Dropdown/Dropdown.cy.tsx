ae3ac4caec081eaeb9bd650c8ba9938c2dd18dfa
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-02 08:28:10 -0400

    Added tests for dropdown

--- src/components/Dropdown/Dropdown.cy.tsx
+++ src/components/Dropdown/Dropdown.cy.tsx
@@ -1,8 +1,33 @@
 // UPDATE IFRAME URL
 describe('DropDown', () => {
-  it('Does Button exist?', () => {
-    cy.visit('/iframe.html?id=component--default&viewMode=story');
-    cy.get('button').should('exist');
+  it('Does DropDown exist?', () => {
+    cy.visit('/iframe.html?id=component--dropdown&viewMode=story');
+    cy.get('div').should('exist');
+    cy.get('sr-only').should('exist');
+    cy.get('p').should('exist');
+    cy.get('svg').should('exist');
+  });
+
+  it('Button component inside dropdown', () => {
+    cy.visit('/iframe.html?id=components-dropdown-button&viewMode=story');
+    cy.get('sr-only').should('exist');
+    cy.get(`div`).should('exist');
+    cy.get(`button`).should('exist');
+  });
+
+  it('Button component with arrow inside dropdown', () => {
+    cy.visit('/iframe.html?id=components-dropdown-button&viewMode=story');
+    cy.get('sr-only').should('exist');
+    cy.get(`div`).should('exist');
+    cy.get(`svg`).should('exist');
+    cy.get(`button`).should('exist');
+  });
+
+  it('Icon component inside dropdown', () => {
+    cy.visit('/iframe.html?id=components-dropdown-button&viewMode=story');
+    cy.get('sr-only').should('exist');
+    cy.get(`div`).should('exist');
+    cy.get(`svg`).should('exist');
   });
 });
 export {};
