959cac5c0e125375dec3e6f5a7d9b78939779e6c
Ish <ishdeep.711@gmail.com>
2022-11-02 13:08:34 -0400

    added link and link wrapper

--- src/components/LinkWrapper/LinkWrapper.tsx
+++ src/components/LinkWrapper/LinkWrapper.tsx
@@ -1,28 +1,28 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 
 export interface LinkProps {
-    link?: string
-    color?: 'black' | 'blue' | 'red'
-    noBorder?: boolean
-    children?: React.ReactNode
+  link?: string;
+  color?: 'black' | 'blue' | 'red';
+  noBorder?: boolean;
+  children?: React.ReactNode;
 }
 
 const LinkWrapper: FC<LinkProps> = ({
-    link,
-    children,
-    color = 'blue',
-    noBorder,
+  link,
+  children,
+  color = 'blue',
+  noBorder,
 }): JSX.Element => {
-    if (link)
-        return (
-            <a
-                href={link}
-                itemProp='url'
-                className={`link--${color} ${noBorder ? `link--noborder` : ``}`}
-            >
-                {children}
-            </a>
-        )
-    return <div>{children}</div>
-}
-export default LinkWrapper
+  if (link)
+    return (
+      <a
+        href={link}
+        itemProp="url"
+        className={`link--${color} ${noBorder ? `link--noborder` : ``}`}
+      >
+        {children}
+      </a>
+    );
+  return <div>{children}</div>;
+};
+export default LinkWrapper;
