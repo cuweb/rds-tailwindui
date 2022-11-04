96eb6715c01bf745779330b5d07e050d5f9e06c7
Ish <ishdeep.711@gmail.com>
2022-11-04 10:39:39 -0400

    updating the command pallet

--- src/components/CommandPallet/CommandPallet.tsx
+++ src/components/CommandPallet/CommandPallet.tsx
@@ -1,6 +1,7 @@
-import { Transition, Dialog, Combobox } from '@headlessui/react';
+import { Combobox, Dialog, Transition } from '@headlessui/react';
 import MagnifyingGlassIcon from '@heroicons/react/24/solid/MagnifyingGlassIcon';
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
+import useRouter from 'next/route';
 
 export interface peopleProps {
   id: number;
@@ -310,69 +311,116 @@ const people: peopleProps[] = [
   },
 ];
 
-function classNames(...classes: any) {
+function classNames(...classes: (string | boolean)[]) {
   return classes.filter(Boolean).join(' ');
 }
+export interface CommandPalletProps {
+  searchDatabase?: any;
+  searchOn?: string;
+}
 
-export default function CommandPallet() {
+export default function CommandPallet({
+  searchOn,
+  searchDatabase,
+}: CommandPalletProps) {
   const [query, setQuery] = useState('');
 
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
+
+  console.log(' open value for command pallet  ', open);
 
   const filteredPeople =
     query === ''
       ? []
       : people.filter(person => {
-          return person?.name.toLowerCase().includes(query.toLowerCase());
+          return person.name.toLowerCase().includes(query.toLowerCase());
         });
 
   return (
-    <div className="relative ">
-      <div className=" inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+    <>
+      <button className="border border-cu-red" onClick={() => setOpen(true)}>
+        <MagnifyingGlassIcon
+          className=" cursor-pointer left-4 h-5 w-5 text-gray-400"
+          aria-hidden="true"
+        />
+      </button>
 
-      <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
-        <div className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
-          <Combobox>
-            <div className="relative">
-              <MagnifyingGlassIcon
-                className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-              <Combobox.Input
-                className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
-                placeholder="Search..."
-                onChange={event => setQuery(event.target.value)}
-              />
-            </div>
+      <Transition.Root
+        show={open}
+        as={Fragment}
+        afterLeave={() => setQuery('')}
+        appear
+      >
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+          </Transition.Child>
 
-            {filteredPeople.length > 0 && (
-              <Combobox.Options
-                static
-                className="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
-              >
-                {filteredPeople.map(person => (
-                  <Combobox.Option
-                    key={person.id}
-                    value={person}
-                    className={({ active }) =>
-                      classNames(
-                        'cursor-default select-none px-4 py-2',
-                        active && 'bg-indigo-600 text-white'
-                      )
-                    }
-                  >
-                    {person.name}
-                  </Combobox.Option>
-                ))}
-              </Combobox.Options>
-            )}
+          <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+                <Combobox onChange={person => (window.location = person?.url)}>
+                  <div className="relative">
+                    <MagnifyingGlassIcon
+                      className="pointer-events-none absolute top-3.5 left-4 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <Combobox.Input
+                      className="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm"
+                      placeholder="Search..."
+                      onChange={event => setQuery(event.target.value)}
+                    />
+                  </div>
 
-            {query !== '' && filteredPeople.length === 0 && (
-              <p className="p-4 text-sm text-gray-500">No people found.</p>
-            )}
-          </Combobox>
-        </div>
-      </div>
-    </div>
+                  {filteredPeople.length > 0 && (
+                    <Combobox.Options
+                      static
+                      className="max-h-72 scroll-py-2 overflow-y-auto py-2 text-sm text-gray-800"
+                    >
+                      {filteredPeople.map(person => (
+                        <Combobox.Option
+                          key={person.id}
+                          value={person}
+                          className={({ active }) =>
+                            classNames(
+                              'cursor-default select-none px-4 py-2',
+                              active && 'bg-indigo-600 text-white'
+                            )
+                          }
+                        >
+                          {person.name}
+                        </Combobox.Option>
+                      ))}
+                    </Combobox.Options>
+                  )}
+
+                  {query !== '' && filteredPeople.length === 0 && (
+                    <p className="p-4 text-sm text-gray-500">
+                      No people found.
+                    </p>
+                  )}
+                </Combobox>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition.Root>
+    </>
   );
 }
