5d71bb85f48e1c6877d45c14f0c134b5acee3381
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-08 08:32:16 -0500

    Updated options, integrated into stacked lists

--- src/components/Pagination/Pagination.stories.tsx
+++ src/components/Pagination/Pagination.stories.tsx
@@ -16,3 +16,24 @@ Default.args = {
   siblingCount: 1,
   pageSize: 5,
 };
+
+export const Border = Template.bind({});
+Border.args = {
+  hasBorder: true,
+  totalCount: 48,
+  siblingCount: 1,
+  pageSize: 5,
+};
+
+export const Spacing = Template.bind({});
+Spacing.args = {
+  hasBorder: true,
+  hasSpacing: true,
+  totalCount: 48,
+  siblingCount: 1,
+  pageSize: 5,
+};
+
+Default.storyName = 'Default item';
+Border.storyName = 'With Top Border';
+Spacing.storyName = 'With Spacing';
--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -3,16 +3,16 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import usePagination, { DOTS } from '../../hooks/usePagination';
 
 export interface PaginationProps {
-  border?: 'none' | 'top' | 'full';
+  hasBorder?: boolean;
+  hasSpacing?: boolean;
   totalCount: number;
   siblingCount: number;
   pageSize: number;
 }
 
-// will be used as options for border?: 'none' | 'top' | 'full'
-
 export const Pagination = ({
-  border = 'none',
+  hasBorder,
+  hasSpacing,
   totalCount,
   siblingCount,
   pageSize,
@@ -56,19 +56,21 @@ export const Pagination = ({
     endResult = currentPage * pageSize;
   }
 
-  console.log(border);
-
   const styles = {
-    'border-none': ``,
-    'border-full': `pl-5 pr-3 py-3 border border-cu-black-100 rounded-lg`,
-    'border-top': `py-5 border-t border-cu-black-100`,
+    border: `py-5 border-t border-cu-black-100`,
+    spacing: `pl-6 pr-5`,
     mobileButtons: `relative inline-flex items-center px-4 py-2 text-sm font-medium text-cu-black-600 bg-white border border-cu-black-200 rounded-md hover:bg-cu-black-50`,
     pageListNumbers: `flex items-center bg-white py-2 px-4 text-sm text-cu-black-600 border-r border-cu-black-100 last:border-0 cursor-pointer hover:bg-cu-black-50`,
     pageListArrows: `text-cu-black-400 px-2.5`,
   };
 
+  const borderStyles = hasBorder ? styles.border : '';
+  const spacingStyles = hasSpacing ? styles.spacing : '';
+
   return (
-    <div className={`flex items-center justify-between`}>
+    <div
+      className={`flex items-center justify-between ${borderStyles} ${spacingStyles}`}
+    >
       <div className="flex justify-between flex-1 sm:hidden">
         <a onClick={onPrevious} className={`${styles.mobileButtons}`}>
           Previous
