06b042a94130789083b91f9c7aa447de3ba36551
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-09 22:56:07 -0500

    Added font size options

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { rdsFontSizes } from '../../utils/tailwindClasses';
 
 export interface BannerProps {
   children: React.ReactNode;
   title?: string;
+  fontSize?: '4xl' | '5xl' | '6xl';
 }
 
-const BannerBase = ({ children, title }: BannerProps) => {
+const BannerBase = ({ children, title, fontSize = '4xl' }: BannerProps) => {
   return (
     <header className="relative bg-gradient-to-b from-white to-cu-black-100">
-      <div className="relative flex flex-col items-center justify-between gap-8 px-8 py-8 mx-auto max-w-7xl lg:py-16 lg:px-8">
-        <h1 className="text-3xl font-medium text-cu-black-800 lg:text-4xl">
+      <div className="relative flex flex-col items-center justify-between max-w-5xl gap-8 px-8 py-8 mx-auto lg:py-16 lg:px-8">
+        <h1
+          className={`text-3xl font-medium text-cu-black-800 text-center ${rdsFontSizes[fontSize]}`}
+        >
           {title}
         </h1>
         {children}
@@ -25,3 +29,6 @@ const ButtonGroup = ({ children }: BannerProps) => {
 export const Banner = Object.assign(BannerBase, {
   ButtonGroup,
 });
+
+BannerBase.displayName = 'Banner';
+ButtonGroup.displayName = 'Banner.ButtonGroup';
