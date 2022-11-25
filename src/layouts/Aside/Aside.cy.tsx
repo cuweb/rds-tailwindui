5ef649b244b59db992978b6db2c47cf936ec2015
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-25 15:05:59 -0500

    Updated tests

--- src/layouts/Aside/Aside.cy.tsx
+++ src/layouts/Aside/Aside.cy.tsx
@@ -1,9 +1,13 @@
 // UPDATE IFRAME URL
 describe('Prose', () => {
-  it('Does Prose exist?', () => {
-    cy.visit('/iframe.html?id=components-typography-prose--article');
-    cy.get('.prose').should('exist');
-    cy.get('img').should('have.css', 'border-radius', '8px');
+  it('Does Aside exist?', () => {
+    cy.visit('/iframe.html?id=layouts-aside--default');
+    cy.get('aside').should('exist');
+  });
+
+  it('Is Aside sticky?', () => {
+    cy.visit('/iframe.html?id=layouts-aside--default');
+    cy.get('.sticky').should('exist');
   });
 });
 export {};
