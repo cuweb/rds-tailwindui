49acd3045810c883d747999a256b1d399d2974d1
Troy Chaplin <troy.chaplin@gmail.com>
2022-11-25 14:56:41 -0500

    Added aside component

---
+++ src/layouts/Aside/Aside.stories.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Aside, AsideProps } from './Aside';
+import { Column } from '../Column';
+import { Prose } from '../../components/Prose';
+import { ProseData as sampleContent } from '../../components/Prose/ProseData';
+
+export default {
+  title: 'Layouts/Aside',
+  component: Aside,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+} as Meta<typeof Aside>;
+
+const DefaultTemplate: Story<AsideProps> = () => (
+  <Aside>
+    <p>This is an aside</p>
+  </Aside>
+);
+
+export const Default = DefaultTemplate.bind({});
+Default.args = {};
+
+const IsStickyTemplate: Story<AsideProps> = args => (
+  <Column cols="2/3" maxWidth="5xl">
+    <Prose as="article" content={sampleContent} />
+    <Aside {...args}>
+      <p>Test</p>
+    </Aside>
+  </Column>
+);
+
+export const IsSticky = IsStickyTemplate.bind({});
+IsSticky.args = {
+  isSticky: true,
+  topSpace: '8',
+};
