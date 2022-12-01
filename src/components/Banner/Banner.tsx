95fe9d3e3b5e22a6b4b0ad26bcc15d3a819b9dc8
Troy Chaplin <troy.chaplin@gmail.com>
2022-12-01 10:58:54 -0500

    Refactored markup and styles

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -8,91 +8,88 @@ export interface BannerProps {
   paragraph?: string;
   fontSize?: '4xl' | '5xl' | '6xl';
   isType?: 'light-fade' | 'dark-wave' | 'red-wave' | 'image' | 'animated';
-  alignment?: 'vertical' | 'horizontal';
+  align?: 'vertical' | 'horizontal';
   hasOverlap?: boolean;
   image?: string;
   imageAlt?: string;
 }
 
-export interface BannerImageProps {
+export interface BannerImgProps {
   image?: string;
   imageAlt?: string;
 }
 
-const styles = {
-  header: `relative`,
-  headerGrey: `bg-gradient-to-b from-white to-cu-black-100 text-cu-black-800`,
-  headerRed: `bg-gradient-to-b from-cu-red to-cu-red-900 text-white`,
-  headerBlack: `bg-cu-black-800 text-white`,
-  overlap: `[&>*]:pb-20 [&+main]:-mt-12 md:[&>*]:pb-44 md:[&+main]:-mt-28 [&+main]:relative [&+main]:z-20`,
-  headerAnimated: `bg-white text-cu-black-800`,
-  childWrapper: `mx-auto flex max-w-7xl flex-col gap-4 md:gap-8 items-center text-center [&>*]:justify-center [&>*]:z-10 [&>*:last-child]:z-0 px-6 md:px-8 py-8 md:py-20`,
-  verticalAlign: `md:flex-col`,
-  horizontalAlign: `md:flex-row md:text-left md:[&>.buttons]:justify-end`,
-  textWrapper: `flex flex-col gap-4 md:gap-6 md:flex-1`,
-  buttonWrapper: `flex gap-6 flex-wrap md:flex-1`,
+const typeStyles = {
+  'light-fade': `bg-gradient-to-b from-white to-cu-black-100 text-cu-black-800`,
+  'dark-wave': `bg-cu-black-800 text-white`,
+  'red-wave': `bg-gradient-to-b from-cu-red to-cu-red-900 text-white`,
+  animated: `bg-white text-cu-black-800`,
+  image: `bg-cu-black-800 text-white`,
+};
+
+const contentStyles = {
   title: `font-semibold text-xl`,
   paragraph: `text-base lg:text-xl max-w-5xl`,
+};
+
+const otherStyles = {
+  buttonWrapper: `flex gap-6 flex-wrap md:flex-1`,
+  overlap: `[&>*]:pb-20 [&+main]:-mt-12 md:[&>*]:pb-44 md:[&+main]:-mt-28 [&+main]:relative [&+main]:z-20`,
 };
 
 const BannerBase = ({
   children,
   title,
   paragraph,
-  isType,
+  isType = 'light-fade',
   fontSize = '4xl',
-  alignment = 'vertical',
+  align = 'vertical',
   hasOverlap,
   image,
   imageAlt = 'Decorative background image',
-}: BannerProps & BannerImageProps) => {
-  let bgStyle;
-  switch (isType) {
-    case 'image':
-      bgStyle = styles.headerBlack;
-      break;
-    case 'dark-wave':
-      bgStyle = styles.headerBlack;
-      break;
-    case 'red-wave':
-      bgStyle = styles.headerRed;
-      break;
-    case 'animated':
-      bgStyle = styles.headerAnimated;
-      break;
-    default:
-      bgStyle = styles.headerGrey;
-  }
-
+}: BannerProps & BannerImgProps) => {
   const bannerSpacing = isType === 'image' ? 'md:py-48' : 'md:py-20';
-  const overlapStyles = hasOverlap ? styles.overlap : '';
-  const alignmentStyles =
-    alignment === 'vertical' ? styles.verticalAlign : styles.horizontalAlign;
+  const flexAlign =
+    align === 'vertical'
+      ? 'flex-col last:[&>*]:justify-center'
+      : 'flex-col md:flex-row last:[&>*]:justify-center last:md:[&>*]:justify-end';
+
+  const hasParagraph = paragraph ? ' md:w-8/12' : '';
+
+  const contentAlign =
+    align === 'vertical'
+      ? 'text-center'
+      : 'text-center md:text-left md:max-w-3xl' + hasParagraph;
 
   return (
-    <header className={`${styles.header} ${overlapStyles} ${bgStyle}`}>
-      <div
-        className={`${styles.childWrapper} ${alignmentStyles} ${bannerSpacing}`}
+    <>
+      <header
+        className={`relative ${typeStyles[isType]} px-6 md:px-8 py-8 ${bannerSpacing}`}
       >
-        <div className={styles.textWrapper}>
-          <h1 className={`${styles.title} ${rdsFontSizes[fontSize]}`}>
-            {title}
-          </h1>
-          {paragraph && <p className={styles.paragraph}>{paragraph}</p>}
+        <div
+          className={`z-10 flex ${flexAlign} items-center justify-center gap-8 m-auto max-w-7xl [&>*]:z-10`}
+        >
+          <div className={`flex flex-col gap-4 ${contentAlign}`}>
+            <h1 className={`${contentStyles.title} ${rdsFontSizes[fontSize]}`}>
+              {title}
+            </h1>
+            {paragraph && (
+              <p className={contentStyles.paragraph}>{paragraph}</p>
+            )}
+          </div>
+          {children}
         </div>
-        {children}
-        {isType === 'image' && (
-          <BannerImage image={image} imageAlt={imageAlt} />
-        )}
-        {isType === 'red-wave' && <BannerWave />}
+
+        {isType === 'image' && <BannerImg image={image} imageAlt={imageAlt} />}
         {isType === 'dark-wave' && <BannerWave />}
+        {isType === 'red-wave' && <BannerWave />}
         {isType === 'animated' && <BannerAnimated />}
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
-const BannerImage = ({ image, imageAlt }: BannerImageProps) => {
+const BannerImg = ({ image, imageAlt }: BannerImgProps) => {
   return (
     <div className="absolute inset-0">
       <img className="object-cover w-full h-full" src={image} alt={imageAlt} />
@@ -111,7 +108,9 @@ const BannerWave = () => {
 };
 
 const ButtonGroup = ({ children }: BannerProps) => {
-  return <div className={`buttons ${styles.buttonWrapper}`}>{children}</div>;
+  return (
+    <div className={`buttons ${otherStyles.buttonWrapper}`}>{children}</div>
+  );
 };
 
 export const Banner = Object.assign(BannerBase, {
