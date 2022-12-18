3df6585c3a4f651fa5ab5e07407374dc617f7f32
Troy Chaplin <troy.chaplin@gmail.com>
2022-12-18 16:00:35 -0500

    Removed unused asset from tw config

--- tailwind.theme.js
+++ tailwind.theme.js
@@ -24,7 +24,6 @@ module.exports = {
             }),
             backgroundImage: {
                 'cu-waves-red': "url('../src/assets/cu-waves-red.svg')",
-                'cu-waves-white-20': "url('../src/assets/cu-waves-white-20.svg')",
                 'cu-waves-white-10': "url('../src/assets/cu-waves-white-10.svg')",
             },
             colors: {
