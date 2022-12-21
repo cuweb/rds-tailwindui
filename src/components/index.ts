c0ce7543add3ab82e056d3c767ce9e37d8bf6e85
Troy Chaplin <troy.chaplin@gmail.com>
2022-12-21 12:08:08 -0500

    Added Card loading component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,18 +1,19 @@
 export { Badge, BadgeProps } from './Badge/Badge';
 export { Button, ButtonProps } from './Button/Button';
 export { Banner, BannerProps, BannerImgProps } from './Banner';
-export { Calendar, CalendarProps } from './Calendar/Calendar';
+export { Calendar, CalendarProps } from './Calendar';
+export { CardLoader } from './Loaders/CardLoader';
 export { DropDown, DropDownItemProps, DropDownProps } from './DropDown';
 export { Filter, FilterProps } from './Filter/Filter';
 export { FooterBasic } from './Footer/FooterBasic';
 export { FooterStandard } from './Footer/FooterStandard';
 export { FooterWave } from './Footer/FooterWave';
-export { EventCard, EventCardProps } from './Cards/EventCard/EventCard';
+export { EventCard, EventCardProps } from './Cards/EventCard';
 export {
   EventLocation,
   EventLocationProps,
 } from './EventLocation/EventLocation';
-export { EventItem, EventItemProps } from './ListingItems/EventItem/EventItem';
+export { EventItem, EventItemProps } from './ListingItems/EventItem';
 export { DetailsItem, DetailsItemProps } from './ListingItems/DetailsItem';
 export { FeedItem, FeedItemProps } from './ListingItems/FeedItem';
 export { FileItem, FileItemProps } from './ListingItems/FileItem';
