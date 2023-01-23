805d16b2c98fea9f724603141d9b892f9eb19e6f
Ish <ishdeep.711@gmail.com>
2023-01-23 14:14:56 -0500

    adding the modal to the rds component

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Meta, Story } from '@storybook/react';
+
+import { Modal, ModalProps } from './Modal';
+import { Button } from '../Button';
+
+export default {
+  title: 'Components/Inputs/Modal',
+  component: Modal,
+  argTypes: {},
+  parameters: {
+    controls: { expanded: true },
+  },
+} as Meta<typeof Modal>;
+
+export const ModalDefault: Story<ModalProps> = () => {
+  const [ModalOpen, setModalOpen] = useState(false);
+
+  return (
+    <>
+      <Button title=" Click to Open Modal" onClick={() => setModalOpen(true)} />
+
+      <Modal
+        title="Modal Title"
+        description={'Description inside the modal'}
+        isOpen={ModalOpen}
+        setIsOpen={setModalOpen}
+      >
+        <Button
+          onClick={() => {
+            alert(' delete are you sure you want to delete it');
+            setModalOpen(false);
+          }}
+          title="Delete"
+          size="sm"
+          hasShadow
+        />
+      </Modal>
+    </>
+  );
+};
