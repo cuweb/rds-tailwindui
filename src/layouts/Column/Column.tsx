dce5031529b7e7078ee9767febaeb2ee1deb1577
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-25 08:06:56 -0500

    Added 4xl and 6xl options to column

--- src/layouts/Column/Column.tsx
+++ src/layouts/Column/Column.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {
-  rdsMaxWidth,
   rdsGridColumns,
   rdsGridSpacing,
+  rdsMaxWidth,
 } from '../../utils/tailwindClasses';
 
 const styles = {
@@ -11,7 +11,7 @@ const styles = {
 
 export interface ColumnProps {
   children: React.ReactNode;
-  maxWidth?: 'none' | 'full' | '3xl' | '5xl' | '7xl';
+  maxWidth?: 'none' | 'full' | '3xl' | '4xl' | '5xl' | '6xl' | '7xl';
   gridGap?: '0' | '0.5' | '1' | '2' | '4' | '8' | '10';
   cols?: '1' | '2' | '3' | '4' | '1/3' | '2/3';
   isNested?: boolean;
