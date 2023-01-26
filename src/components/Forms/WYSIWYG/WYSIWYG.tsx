b2feaefdc035bd4d4ab9392a691793da947ca008
Ish <ishdeep.711@gmail.com>
2023-01-26 09:58:10 -0500

    adding options to react quill

--- src/components/Forms/WYSIWYG/WYSIWYG.tsx
+++ src/components/Forms/WYSIWYG/WYSIWYG.tsx
@@ -5,5 +5,54 @@ import 'react-quill/dist/quill.snow.css';
 export const WYSIWYG = () => {
   const [value, setValue] = useState('');
 
-  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+  const modules = {
+    toolbar: [
+      [{ header: '1' }, { header: '2' }, { font: [] }],
+      [{ size: [] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ],
+      ['link', 'image', 'video'],
+      ['clean'],
+    ],
+    clipboard: {
+      // toggle to add extra line breaks when pasting HTML:
+      matchVisual: false,
+    },
+  };
+  /*
+   * Quill editor formats
+   * See https://quilljs.com/docs/formats/
+   */
+  const formats = [
+    'header',
+    'font',
+    'size',
+    'bold',
+    'italic',
+    'underline',
+    'strike',
+    'blockquote',
+    'list',
+    'bullet',
+    'indent',
+    'link',
+    'image',
+    'video',
+  ];
+
+  return (
+    <ReactQuill
+      theme="snow"
+      value={value}
+      onChange={setValue}
+      placeholder="write some description...."
+      modules={modules}
+      formats={formats}
+    />
+  );
 };
