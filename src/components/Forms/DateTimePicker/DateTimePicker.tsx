2bcb7cf6d9b8698bb2d8fc60de4e15d53d95cd8e
Troy Chaplin <troy.chaplin@gmail.com>
2023-01-27 16:12:23 -0500

    Fixed a11y bg image issue

--- src/components/Forms/DateTimePicker/DateTimePicker.tsx
+++ src/components/Forms/DateTimePicker/DateTimePicker.tsx
@@ -1,10 +1,11 @@
+import { ChevronDownIcon } from '@heroicons/react/24/solid';
 import { format } from 'date-fns';
 import { FieldHookConfig, useField } from 'formik';
 import React, { useCallback, useEffect, useState } from 'react';
 import { formStyles } from '../../../utils/formClasses';
 import { Calendar } from '../../Calendar/Calendar';
 const styles = {
-  select: `text-xs bg-white rounded-md outline-none appearance-none border-cu-black-100 text-cu-black-900 focus:border-red-500 focus:ring-0`,
+  select: `text-xs bg-none rounded-md outline-none appearance-none border-cu-black-100 text-cu-black-900 focus:border-red-500 focus:ring-0`,
 };
 export interface PickerProps {
   label?: string;
@@ -67,60 +68,75 @@ export const DateTimePicker = ({
       >
         <Calendar callback={callbackcal} />
         <div className="inline-flex gap-3 p-3 mt-6 bg-white border rounded-lg border-cu-black-100">
-          <label htmlFor="field-hours" className="sr-only">
-            {label} {props.required && <span className="text-cu-red">*</span>}
-          </label>
-          <select
-            id="field-hours"
-            value={hours}
-            onChange={handleHoursChange}
-            name="hours"
-            className={styles.select}
-          >
-            {Array.from({ length: 12 }, (_, i) => i + 1).map(num => (
-              <option key={num} value={num}>
-                {num < 10 ? '0' + num : num}
+          <div className="relative">
+            <label htmlFor="field-hours" className="sr-only">
+              {label} {props.required && <span className="text-cu-red">*</span>}
+            </label>
+            <select
+              id="field-hours"
+              value={hours}
+              onChange={handleHoursChange}
+              name="hours"
+              className={styles.select}
+            >
+              {Array.from({ length: 12 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>
+                  {num < 10 ? '0' + num : num}
+                </option>
+              ))}
+            </select>
+            <div className="absolute top-3 right-2">
+              <ChevronDownIcon width="12" height="12" />
+            </div>
+          </div>
+
+          <div className="relative">
+            <label htmlFor="field-minutes" className="sr-only" />
+            <select
+              id="field-minutes"
+              value={minutes}
+              onChange={handleMinutesChange}
+              name="minutes"
+              className={styles.select}
+            >
+              <option key="00" value="00">
+                00
               </option>
-            ))}
-          </select>
+              <option key="15" value="15">
+                15
+              </option>
+              <option key="30" value="30">
+                30
+              </option>
+              <option key="45" value="45">
+                45
+              </option>
+            </select>
+            <div className="absolute top-3 right-2">
+              <ChevronDownIcon width="12" height="12" />
+            </div>
+          </div>
 
-          <label htmlFor="field-minutes" className="sr-only" />
-          <select
-            id="field-minutes"
-            value={minutes}
-            onChange={handleMinutesChange}
-            name="minutes"
-            className={styles.select}
-          >
-            <option key="00" value="00">
-              00
-            </option>
-            <option key="15" value="15">
-              15
-            </option>
-            <option key="30" value="30">
-              30
-            </option>
-            <option key="45" value="45">
-              45
-            </option>
-          </select>
-
-          <label htmlFor="field-ampm" className="sr-only" />
-          <select
-            id="field-ampm"
-            value={noon}
-            onChange={handleNoonChange}
-            name="ampm"
-            className={styles.select}
-          >
-            <option key="AM" value="AM">
-              AM
-            </option>
-            <option key="PM" value="PM">
-              PM
-            </option>
-          </select>
+          <div className="relative">
+            <label htmlFor="field-ampm" className="sr-only" />
+            <select
+              id="field-ampm"
+              value={noon}
+              onChange={handleNoonChange}
+              name="ampm"
+              className={styles.select}
+            >
+              <option key="AM" value="AM">
+                AM
+              </option>
+              <option key="PM" value="PM">
+                PM
+              </option>
+            </select>
+            <div className="absolute top-3 right-2">
+              <ChevronDownIcon width="12" height="12" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
