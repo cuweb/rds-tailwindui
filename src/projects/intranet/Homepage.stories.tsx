7b6c7dd60a2f1f934106c2d10ba302eabe4e5752
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-14 12:05:10 -0500

    Updated intranet template, release 0.7.1

--- src/projects/intranet/Homepage.stories.tsx
+++ src/projects/intranet/Homepage.stories.tsx
@@ -15,40 +15,56 @@ export default {
   },
 } as Meta;
 
+const SinglePara = () => {
+  return (
+    <>
+      <p>
+        <strong>Single Paragraph:</strong> Nobis voluptatem dolorum et eum
+        doloremque cupiditate velit. Praesentium architecto a distinctio aut
+        reprehenderit ducimus. Perferendis excepturi delectus nihil voluptatem
+        non. Molestiae quas dolores accusamus in. Praesent quis ligula quis
+        nulla malesuada tempor.
+      </p>
+    </>
+  );
+};
+
+const DoublePara = () => {
+  return (
+    <>
+      <p>
+        <strong>Double Paragraph:</strong> Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit. Aenean sit amet tortor pellentesque,
+        posuere tellus vitae, sagittis justo. Vivamus imperdiet turpis nec elit
+        ultricies, sed tempus diam dignissim. Suspendisse condimentum magna vel
+        orci vulputate, eget vulputate neque porttitor. Suspendisse euismod,
+        urna et gravida volutpat, tortor risus vehicula nisl, in vulputate
+        lectus dolor viverra est. Etiam quis interdum nisi, et malesuada lectus.
+        Aliquam luctus, velit eget suscipit tincidunt, sem ex tempus turpis,
+        quis pulvinar metus sapien in urna.
+      </p>
+      <p>
+        Nobis voluptatem dolorum et eum doloremque cupiditate velit. Praesentium
+        architecto a distinctio aut reprehenderit ducimus. Perferendis excepturi
+        delectus nihil voluptatem non. Molestiae quas dolores accusamus in.
+        Praesent quis ligula quis nulla malesuada tempor.
+      </p>
+    </>
+  );
+};
+
 export const Homepage = () => (
   <>
     <TopNav title="Carleton University" />
-    <Banner title="Template" />
+    <Banner title="Intranet Homepage" />
 
     <Main>
-      <Container as="section" hasProse maxWidth="7xl">
-        <p>safhasufhasiuo</p>
-
-        <Column cols="2/3" maxWidth="7xl">
-          <Container as="article" hasProse>
-            <p>safhasufhasiuo</p>
-          </Container>
-        </Column>
-
-        <Column cols="2/3" maxWidth="7xl">
-          <Container as="article" hasProse>
-            <p>safhasufhasiuo</p>
-          </Container>
-        </Column>
-
-        <Container bgColor="grey" as="div">
-          <p>safhasufhasiuo</p>
-        </Container>
-
-        <Column cols="2/3" maxWidth="7xl">
-          <Container as="article" hasProse>
-            <p>safhasufhasiuo</p>
-          </Container>
-        </Column>
-
-        <Container as="div" bgColor="grey">
-          <p>safhasufhasiuo</p>
-        </Container>
+      <Container hasProse>
+        <SinglePara />
+        <h2>This is a header two</h2>
+        <DoublePara />
+        <h3>This is a header three</h3>
+        <SinglePara />
       </Container>
     </Main>
 
