059606937a4fc882e7bbe0c37ee7643bb919b559
Mounika Vojjala <mounikavojjala@gmail.com>
2023-02-14 09:55:28 -0500

    fixed video player without thumbnail and modal

--- src/components/Cards/VideoCard/VideoCard.tsx
+++ src/components/Cards/VideoCard/VideoCard.tsx
@@ -1,51 +1,51 @@
-import React, { useState } from 'react';
-import { PlayCircleIcon } from '@heroicons/react/24/solid';
+import React from 'react';
+// import { PlayCircleIcon } from '@heroicons/react/24/solid';
 import ReactPlayer from 'react-player';
-import { VideoModal } from '../../VideoModal';
+// import { VideoModal } from '../../VideoModal';
 
 export interface VideoCardProps {
   source?: any;
 }
 
 export function VideoCard({ source }: VideoCardProps) {
-  const [ModalOpen, setModalOpen] = useState(false);
-  const [urlThumbnail, seturlThumbnail] = useState('');
+  // const [ModalOpen, setModalOpen] = useState(false);
+  // const [urlThumbnail, seturlThumbnail] = useState('');
 
-  const getYouTubeVideoImage = (url: string, size: any) => {
-    const imageSize = size === 'small' ? 2 : 0;
-    const regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    const videoId = match && match[1].length == 11 ? match[1] : false;
-    return `https://img.youtube.com/vi/${videoId}/${imageSize}.jpg`;
-  };
+  // const getYouTubeVideoImage = (url: string, size: any) => {
+  //   const imageSize = size === 'small' ? 2 : 0;
+  //   const regExp = /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/;
+  //   const match = url.match(regExp);
+  //   const videoId = match && match[1].length == 11 ? match[1] : false;
+  //   return `https://img.youtube.com/vi/${videoId}/${imageSize}.jpg`;
+  // };
 
-  const getVimeoVideoImage = (url: string) => {
-    fetch(`https://vimeo.com/api/oembed.json?url=${url}`)
-      .then(res => res.json())
-      .then(data => {
-        seturlThumbnail(data.thumbnail_url);
-      })
-      .catch(err => console.log(err));
+  // const getVimeoVideoImage = (url: string) => {
+  //   fetch(`https://vimeo.com/api/oembed.json?url=${url}`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       seturlThumbnail(data.thumbnail_url);
+  //     })
+  //     .catch(err => console.log(err));
 
-    return urlThumbnail;
-  };
-  const videoThumbnail = (video_url: string, size: any) => {
-    let link = video_url.toString();
-    if (link.includes('vimeo')) {
-      return getVimeoVideoImage(video_url);
-    }
-    return getYouTubeVideoImage(video_url, size);
-  };
+  //   return urlThumbnail;
+  // };
+  // const videoThumbnail = (video_url: string, size: any) => {
+  //   let link = video_url.toString();
+  //   if (link.includes('vimeo')) {
+  //     return getVimeoVideoImage(video_url);
+  //   }
+  //   return getYouTubeVideoImage(video_url, size);
+  // };
 
   return (
     <>
       <div className="not-prose group relative flex flex-col overflow-hidden rounded-lg bg-white shadow-lg duration-300 ease-in @container hover:scale-105 md:max-w-lg">
         <a
-          onClick={() => setModalOpen(true)}
+          // onClick={() => setModalOpen(true)}
           className="relative flex flex-col h-full cursor-pointer"
         >
           <div className="relative">
-            <img
+            {/* <img
               className="object-cover w-full"
               src={videoThumbnail(source, 'small')}
               alt=""
@@ -57,7 +57,10 @@ export function VideoCard({ source }: VideoCardProps) {
                 aria-hidden="true"
                 color="white"
               />
-            </div>
+            </div> */}
+        
+            <ReactPlayer url={source} controls class="w-full aspect-video"/>
+          
           </div>
 
           <div className="flex flex-col space-y-2 py-6 px-7 @sm:md:space-y-3">
@@ -79,11 +82,12 @@ export function VideoCard({ source }: VideoCardProps) {
           </div>
         </a>
       </div>
-      <div className="overflow-hidden w-[600px]">
+      {/* <div className="overflow-hidden w-[600px]">
         <VideoModal onClose={() => setModalOpen(false)} isVisible={ModalOpen}>
           <ReactPlayer url={source} playing />
         </VideoModal>
-      </div>
+      </div> */}
     </>
   );
 }
+
