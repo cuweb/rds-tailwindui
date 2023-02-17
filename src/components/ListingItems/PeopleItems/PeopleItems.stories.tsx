eded080b711fdc91f661ddde291015485e045a2d
Ish <ishdeep.711@gmail.com>
2023-02-17 14:13:18 -0500

    listing people component with dot notation

--- src/components/ListingItems/PeopleItems/PeopleItems.stories.tsx
+++ src/components/ListingItems/PeopleItems/PeopleItems.stories.tsx
@@ -8,7 +8,7 @@ import { StackedList } from '../../../layouts/StackedList';
 import { PeopleItemsData as data } from './PeopleItemsData';
 
 export default {
-  title: 'Components/List Items/People',
+  title: 'Cards & Lists/People/List Item',
   component: PeopleItems,
   argTypes: {
     fontSize: {
@@ -26,23 +26,23 @@ const styles = {
 };
 
 const DefaultTemplate: Story<PeopleItemsProps> = args => (
-  <PeopleItems {...args} />
+  <PeopleItems as="div" link={args.link}>
+    <PeopleItems.Image
+      profileImage={args.profileImage}
+      firstName={args.firstName}
+      lastName={args.lastName}
+    />
+    <PeopleItems.Details>
+      <PeopleItems.Name firstName={args.firstName} lastName={args.lastName} />
+      <PeopleItems.Designation designation={args.designation} />
+      <PeopleItems.Contact>
+        <PeopleItems.Email email={args.email} />
+        <PeopleItems.PhoneNumber phoneNumber={args.phoneNumber} />
+      </PeopleItems.Contact>
+      <PeopleItems.PeopleTags tags={args.tags} />
+    </PeopleItems.Details>
+  </PeopleItems>
 );
-
-// const SingleItemPanelTemplate: Story<PeopleItemsProps> = args => (
-//   <Column maxWidth="5xl">
-//     <Panel hasBorder>
-//       <PeopleItems as="div" link={args.link}>
-//         <PeopleItems.Image image={args.image} />
-//         <PeopleItems.Content>
-//           <PeopleItems.Title title={args.title} link={args.link} />
-//           <PeopleItems.Date date={args.date} />
-//           <PeopleItems.Excerpt excerpt={args.excerpt} />
-//         </PeopleItems.Content>
-//       </PeopleItems>
-//     </Panel>
-//   </Column>
-// );
 
 const StackedListPanelTemplate: Story<PeopleItemsProps> = () => (
   <Container>
@@ -63,17 +63,26 @@ const StackedListPanelTemplate: Story<PeopleItemsProps> = () => (
               alt,
             }) => (
               <div key={id}>
-                <PeopleItems
-                  firstName={firstName}
-                  lastName={lastName}
-                  email={email}
-                  tags={tags}
-                  profileImage={profileImage}
-                  designation={designation}
-                  phoneNumber={phoneNumber}
-                  link={link}
-                  alt={alt}
-                />
+                <PeopleItems as="div" link={link}>
+                  <PeopleItems.Image
+                    profileImage={profileImage}
+                    firstName={firstName}
+                    lastName={lastName}
+                    alt={alt}
+                  />
+                  <PeopleItems.Details>
+                    <PeopleItems.Name
+                      firstName={firstName}
+                      lastName={lastName}
+                    />
+                    <PeopleItems.Designation designation={designation} />
+                    <PeopleItems.Contact>
+                      <PeopleItems.Email email={email} />
+                      <PeopleItems.PhoneNumber phoneNumber={phoneNumber} />
+                    </PeopleItems.Contact>
+                    <PeopleItems.PeopleTags tags={tags} />
+                  </PeopleItems.Details>
+                </PeopleItems>
               </div>
             )
           )}
@@ -104,17 +113,26 @@ const StackedListTwoColumnTemplate: Story<PeopleItemsProps> = () => (
               alt,
             }) => (
               <div key={id}>
-                <PeopleItems
-                  firstName={firstName}
-                  lastName={lastName}
-                  email={email}
-                  tags={tags}
-                  profileImage={profileImage}
-                  designation={designation}
-                  phoneNumber={phoneNumber}
-                  link={link}
-                  alt={alt}
-                />
+                <PeopleItems as="div" link={link}>
+                  <PeopleItems.Image
+                    profileImage={profileImage}
+                    firstName={firstName}
+                    lastName={lastName}
+                    alt={alt}
+                  />
+                  <PeopleItems.Details>
+                    <PeopleItems.Name
+                      firstName={firstName}
+                      lastName={lastName}
+                    />
+                    <PeopleItems.Designation designation={designation} />
+                    <PeopleItems.Contact>
+                      <PeopleItems.Email email={email} />
+                      <PeopleItems.PhoneNumber phoneNumber={phoneNumber} />
+                    </PeopleItems.Contact>
+                    <PeopleItems.PeopleTags tags={tags} />
+                  </PeopleItems.Details>
+                </PeopleItems>
               </div>
             )
           )}
@@ -123,27 +141,6 @@ const StackedListTwoColumnTemplate: Story<PeopleItemsProps> = () => (
     </Column>
   </Container>
 );
-// const StackedListPanelTitleTemplate: Story<PeopleItemsProps> = () => (
-//   <Container bgColor="grey">
-//     <Column maxWidth="5xl">
-//       <Panel hasBorder hasShadow>
-//         <Panel.Title>News listing</Panel.Title>
-//         <StackedList hasDividers>
-//           {data.map(({ id, title, link, image, date, excerpt }) => (
-//             <PeopleItems key={id}>
-//               <PeopleItems.Image image={image} />
-//               <PeopleItems.Content>
-//                 <PeopleItems.Title title={title} link={link} />
-//                 <PeopleItems.Date date={date} />
-//                 <PeopleItems.Excerpt excerpt={excerpt} />
-//               </PeopleItems.Content>
-//             </PeopleItems>
-//           ))}
-//         </StackedList>
-//       </Panel>
-//     </Column>
-//   </Container>
-// );
 
 export const Default = DefaultTemplate.bind({});
 Default.args = {
@@ -196,23 +193,16 @@ NoImage.args = {
   link: '#',
 };
 
+export const StackedListPanel = StackedListPanelTemplate.bind({});
+StackedListPanel.args = {
+  ...Default.args,
+};
 export const StackedListTwoColumn = StackedListTwoColumnTemplate.bind({});
 StackedListTwoColumn.args = {
   ...Default.args,
 };
 
-export const StackedListPanel = StackedListPanelTemplate.bind({});
-StackedListPanel.args = {
-  ...Default.args,
-};
-
-// export const StackedListPanelTitle = StackedListPanelTitleTemplate.bind({});
-// StackedListPanelTitle.args = {
-//   ...Default.args,
-// };
-
 Default.storyName = 'Default item';
 NoImage.storyName = ' No People Image Item';
 StackedListPanel.storyName = 'List w/shadow';
 StackedListTwoColumn.storyName = 'Two Column Listing ';
-// StackedListPanelTitle.storyName = 'List w/title';
--- src/components/ListingItems/PeopleItems/PeopleItems.tsx
+++ src/components/ListingItems/PeopleItems/PeopleItems.tsx
@@ -1,108 +1,156 @@
+import { ChevronRightIcon } from '@heroicons/react/24/outline';
 import React from 'react';
 import { Tags } from '../../../types/Tags';
 import { Badge } from '../../Badge';
 import { Link } from '../../Link/Link';
 
+type BaseItemTypeProps = 'li' | 'div';
+
 export interface PeopleItemsProps {
-  firstName: string;
-  lastName: string;
+  children?: React.ReactNode;
+  firstName?: string;
+  lastName?: string;
   designation?: string;
-  email: string;
+  email?: string;
   phoneNumber?: string;
   tags?: Tags;
-  profileImage?: any;
+  profileImage?: string;
   link?: string;
   alt?: string;
 }
 
-export const PeopleItems = ({
-  firstName,
-  lastName,
-  email,
-  phoneNumber,
-  tags,
-  designation,
-  profileImage,
-  link = '',
-  alt,
-}: PeopleItemsProps) => {
-  const initials = `${firstName.split('')[0]}${lastName.split('')[0]}`;
+export interface BaseItemProps {
+  as?: BaseItemTypeProps;
+}
+
+const PeopleItemBase = ({
+  as: Component = 'div',
+  children,
+  link,
+}: BaseItemProps & PeopleItemsProps) => {
   return (
-    <div className="not-prose group relative overflow-hidden @container">
+    <Component className="not-prose group relative overflow-hidden @container">
       <Link
         href={link}
-        className="group relative flex cursor-pointer flex-row gap-4 bg-white p-6 hover:bg-slate-50 focus:outline-none md:gap-5 @sm:md:flex-col sm:grid-cols-1 @lg:md:flex-row @lg:lg:gap-8"
+        className="group relative flex cursor-pointer flex-row gap-4 bg-white p-6 hover:bg-slate-50 focus:outline-none md:gap-5 @sm:md:flex-col @lg:md:flex-row @lg:lg:gap-8"
       >
-        {/* Image  */}
-        <div className="h-28 w-28 @sm:md:h-36 @sm:md:w-36 @lg:md:h-60 @lg:md:w-60">
-          {profileImage && (
-            <img
-              className="w-full overflow-hidden rounded-lg border-4 border-white object-cover shadow-md"
-              src={profileImage}
-              alt={alt}
-            />
-          )}
-          {!profileImage && (
-            <div
-              className={` w-full h-full overflow-hidden rounded-lg border-4  object-cover shadow-md flex items-center justify-center font-semibold  @sm:text-xl @lg:text-6xl bg-cu-black-100`}
-            >
-              {initials}
-            </div>
-          )}
-        </div>
-
-        <div className="flex flex-1 flex-col gap-1.5 pr-6 pt-1 md:gap-px @sm:md:pt-0 @lg:md:pt-2">
-          {/* Name  */}
-          <h3 className="text-lg font-semibold text-cu-black group-hover:text-cu-red md:text-xl @lg:md:text-2xl">
-            {firstName + ' ' + lastName}
-          </h3>
-          {/* Designation */}
-          <p className="text-sm italic text-cu-black-600 @md:md:text-base @2xl:md:text-lg">
-            {designation}
-          </p>
-
-          {/* Contact Details   */}
-          <ul className="space-y-1.5 pt-4">
-            <li className="text-sm text-cu-black-700 @2xl:lg:text-base">
-              <a
-                href="mailto:webdevs@carleton.ca"
-                className="font-semibold text-cyan-700 hover:text-cu-red"
-              >
-                {email}
-              </a>
-            </li>
-            <li className="text-sm text-cu-black-700 hover:text-cu-red @2xl:lg:text-base">
-              <a href="#" className="">
-                {phoneNumber}
-              </a>
-            </li>
-          </ul>
-
-          <div className="mt-4 @lg:md:mt-auto">
-            {tags?.category?.map(tag => (
-              <Badge key={tag.id}>{tag.name}</Badge>
-            ))}
-          </div>
-        </div>
+        {children}
 
         <div className="absolute top-1/2 right-4 -mt-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
+          <ChevronRightIcon
+            className="flex-none w-5 h-5 ml-auto text-cu-black-300"
             aria-hidden="true"
-            className="ml-auto h-6 w-6 flex-none text-cu-black-300 group-hover:text-cu-red"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M8.25 4.5l7.5 7.5-7.5 7.5"
-            ></path>
-          </svg>
+          />
         </div>
       </Link>
+    </Component>
+  );
+};
+
+const Image = ({
+  profileImage,
+  alt,
+  firstName,
+  lastName,
+}: PeopleItemsProps) => {
+  const initials = `${firstName?.split('')[0]}${lastName?.split('')[0]}`;
+  return (
+    <div className="h-28 w-28 @sm:md:h-36 @sm:md:w-36 @lg:md:h-60 @lg:md:w-60">
+      {profileImage && (
+        <img
+          className="w-full overflow-hidden rounded-lg border-4 border-white object-cover shadow-md"
+          src={profileImage}
+          alt={alt}
+        />
+      )}
+      {!profileImage && (
+        <div
+          className={` w-full h-full overflow-hidden rounded-lg border-4  object-cover shadow-md flex items-center justify-center font-semibold  @sm:text-xl @lg:text-6xl bg-cu-black-100`}
+        >
+          {initials}
+        </div>
+      )}
     </div>
   );
 };
+
+const Details = ({ children }: PeopleItemsProps) => {
+  return (
+    <div className="flex flex-1 flex-col gap-1.5 pr-6 pt-1 md:gap-px @sm:md:pt-0 @lg:md:pt-2">
+      {children}
+    </div>
+  );
+};
+
+const Name = ({ firstName, lastName }: PeopleItemsProps) => {
+  return (
+    <h3 className="text-lg font-semibold text-cu-black group-hover:text-cu-red md:text-xl @lg:md:text-2xl">
+      {firstName + ' ' + lastName}
+    </h3>
+  );
+};
+const Designation = ({ designation }: PeopleItemsProps) => {
+  return (
+    <p className="text-sm italic text-cu-black-600 @md:md:text-base @2xl:md:text-lg">
+      {designation}
+    </p>
+  );
+};
+
+const Contact = ({ children }: PeopleItemsProps) => {
+  return <ul className="space-y-1.5 pt-4">{children}</ul>;
+};
+
+const Email = ({ email }: PeopleItemsProps) => {
+  return (
+    <li className="text-sm text-cu-black-700 @2xl:lg:text-base">
+      <a
+        href="mailto:webdevs@carleton.ca"
+        className="font-semibold text-cyan-700 hover:text-cu-red"
+      >
+        {email}
+      </a>
+    </li>
+  );
+};
+
+const PhoneNumber = ({ phoneNumber }: PeopleItemsProps) => {
+  return (
+    <li className="text-sm text-cu-black-700 hover:text-cu-red @2xl:lg:text-base">
+      <a href="#" className="">
+        {phoneNumber}
+      </a>
+    </li>
+  );
+};
+
+const PeopleTags = ({ tags }: PeopleItemsProps) => {
+  return (
+    <div className="mt-4 @lg:md:mt-auto ">
+      {tags?.category?.map(tag => (
+        <Badge key={tag.id}>{tag.name}</Badge>
+      ))}
+    </div>
+  );
+};
+
+PeopleItemBase.displayName = 'PeopleItems';
+Image.displayName = 'PeopleItems.Image';
+Name.displayName = 'PeopleItems.Name';
+Designation.displayName = 'PeopleItems.Designation';
+Details.displayName = 'PeopleItems.Details';
+Contact.displayName = 'PeopleItems.Contact';
+PhoneNumber.displayName = 'PeopleItems.PhoneNumber';
+Email.displayName = 'PeopleItems.Email';
+PeopleTags.displayName = 'PeopleItems.PeopleTags';
+
+export const PeopleItems = Object.assign(PeopleItemBase, {
+  Image,
+  Details,
+  Contact,
+  Email,
+  PhoneNumber,
+  Name,
+  Designation,
+  PeopleTags,
+});
