7d1f38f02a85b90c697cb6fa294b9c9ad6d0d4c3
Troy Chaplin <troy.chaplin@gmail.com>
2023-02-21 13:37:08 -0500

    Fixed issues with borders

--- src/layouts/StackedList/StackedList.tsx
+++ src/layouts/StackedList/StackedList.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const styles = {
   wrapper: `cu-panel not-prose overflow-hidden rounded-lg bg-white`,
   title: `px-6 py-4 text-base md:text-xl font-semibold border-b rounded-t-lg bg-gray-50 text-cu-black-800`,
-  grid: `grid overflow-hidden`,
+  grid: `grid overflow-hidden [&>*]:border-cu-black-100 [&>*]:border-b last:[&>*]:border-b-0`,
   border: `border border-cu-black-100`,
   shadow: `shadow-lg`,
 };
 
 const gridStyles = {
-  oneCol: `md:grid-cols-1 [&>*]:border-cu-cu-black-100 [&>*]:border-b last:[&>*]:border-b-0`,
-  twoCol: `md:grid-cols-2 [&>*]:border-t [&>*]:border-cu-black-100 first:[&>*]:border-t-0 md:odd:[&>*]:border-r md:[&>*:nth-child(2)]:border-t-0`,
+  oneCol: `md:grid-cols-1`,
+  twoCol: `md:grid-cols-2 md:odd:[&>*]:border-r`,
 };
 
 export interface StackedListProps {
@@ -35,7 +35,7 @@ export const StackedList = ({
   return (
     <div className={`${styles.wrapper} ${borderStyle} ${shadowStyle}`}>
       {header && <h2 className={`${styles.title}`}>{header}</h2>}
-      <div className={`grid overflow-hidden ${gridColumns}`}>{children}</div>
+      <div className={`${styles.grid} ${gridColumns}`}>{children}</div>
     </div>
   );
 };
