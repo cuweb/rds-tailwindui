0e4b68c3030321d6720e8d55abb1d671a835dfd3
Mounika Vojjala <mounikavojjala@gmail.com>
2023-03-06 14:39:39 -0500

    added duration prop to video card

--- src/components/Cards/VideoCard/VideoCard.stories.tsx
+++ src/components/Cards/VideoCard/VideoCard.stories.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Meta } from '@storybook/react';
 import { VideoCard } from './VideoCard';
-import { Column } from '../../../layouts';
-import { VideoCardData as data } from './VideoCardData';
+// import { Column } from '../../../layouts';
+// import { VideoCardData as data } from './VideoCardData';
 
 export default {
   title: 'Cards & Lists/Video/Card',
@@ -14,8 +14,17 @@ export default {
 } as Meta<typeof VideoCard>;
 
 export const YouTube = () => {
+  const [s, setSelectedDate] = useState(null);
+  const callbackcal = useCallback(
+    (itemSelected: any) => {
+      setSelectedDate(itemSelected);
+    },
+    [setSelectedDate]
+  );
+  console.log('sss', s);
   return (
     <VideoCard
+      callback={callbackcal}
       source="https://www.youtube.com/watch?v=3PUK_wRVzHI"
       tags={{
         category: [
@@ -35,36 +44,36 @@ export const YouTube = () => {
   );
 };
 
-export const Vimeo = () => {
-  return (
-    <VideoCard
-      source="https://vimeo.com/106595658"
-      tags={{
-        category: [
-          {
-            id: 1,
-            name: 'Category One',
-            slug: 'category-one',
-          },
-          {
-            id: 2,
-            name: 'Category Two',
-            slug: 'category-two',
-          },
-        ],
-      }}
-    />
-  );
-};
+// export const Vimeo = () => {
+//   return (
+//     <VideoCard
+//       source="https://vimeo.com/106595658"
+//       tags={{
+//         category: [
+//           {
+//             id: 1,
+//             name: 'Category One',
+//             slug: 'category-one',
+//           },
+//           {
+//             id: 2,
+//             name: 'Category Two',
+//             slug: 'category-two',
+//           },
+//         ],
+//       }}
+//     />
+//   );
+// };
 
-export const GridCards = () => (
-  <Column cols="3" gridGap="10" maxWidth="7xl">
-    {data.map(item => (
-      <VideoCard source={item?.source} tags={item?.tags} />
-    ))}
-  </Column>
-);
+// export const GridCards = () => (
+//   <Column cols="3" gridGap="10" maxWidth="7xl">
+//     {data.map(item => (
+//       <VideoCard source={item?.source} tags={item?.tags} />
+//     ))}
+//   </Column>
+// );
 
 YouTube.storyName = 'YouTube';
-Vimeo.storyName = 'Vimeo';
-GridCards.storyName = 'Grid Card Layout';
+// Vimeo.storyName = 'Vimeo';
+// GridCards.storyName = 'Grid Card Layout';
